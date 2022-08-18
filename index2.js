c8999d606c0951953f76c6231cb5b885acc2bef1
Dacmay29 <dacmay29@outlook.com>
2022-08-18 15:40:41 -0500

    modificado

---
+++ index2.js
@@ -0,0 +1,6 @@
+console.log("David  May git")
+
+
+//let nombre = 'Ezequiel';
+let saludar = nombre => ( ' Hola, '  +  nombre  +  '!');
+console.log(saludar('Ezequiel' + ' el poderoso de la Montaña '))
